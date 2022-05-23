6a6d3dfab7e7bd1ce9e62a6111364d74feba4001
dragosvasile11 <vasile.dragos11@gmail.com>
2022-05-23 17:03:45 +0300

    Implement Footer component.

---
+++ Spoint/src/components/Footer.jsx
@@ -0,0 +1,32 @@
+import React from 'react';
+
+const Footer = () => {
+    return (
+        <>
+            <footer className="bg-dark text-center text-white fixed-bottom" style={{ backgroundColor: 'rgba(0, 0, 0, 0.1)' }} >
+                <div className="container p-4 pb-0" >
+                    <section className="mb-4">
+                        <a className="btn btn-outline-light btn-floating m-1" href="#" role="button"
+                        ><i className="fab fa-facebook-f"/></a>
+                        <a className="btn btn-outline-light btn-floating m-1" href="#" role="button"
+                        ><i className="fab fa-twitter"/></a>
+                        <a className="btn btn-outline-light btn-floating m-1" href="#" role="button"
+                        ><i className="fab fa-google"/></a>
+                        <a className="btn btn-outline-light btn-floating m-1" href="#" role="button"
+                        ><i className="fab fa-instagram"/></a>
+                        <a className="btn btn-outline-light btn-floating m-1" href="#" role="button"
+                        ><i className="fab fa-linkedin-in"/></a>
+                        <a className="btn btn-outline-light btn-floating m-1" href="#" role="button"
+                        ><i className="fab fa-github"/></a>
+                    </section>
+                </div>
+                <div className="text-center p-3" >
+                    © 2022 Copyright:
+                    <a className="text-white"> Army of Two</a>
+                </div>
+            </footer>
+        </>
+    );
+};
+
+export default Footer;
