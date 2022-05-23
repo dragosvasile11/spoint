ccd8b79f4775c2f171efd20b966fefe13898079b
dragosvasile11 <vasile.dragos11@gmail.com>
2022-05-23 16:59:39 +0300

    Create App.jsx file (returning components: Header, Footer).

---
+++ Spoint/src/App.jsx
@@ -0,0 +1,38 @@
+import React from "react";
+
+import Header from "./components/Header";
+import Footer from "./components/Footer";
+
+const App = () => (
+  <>
+      <Header />
+      <div>
+          <p>Checking</p>
+          <p>Checking</p>
+          <p>Checking</p>
+      </div>
+      <div>
+          <p>Checking</p>
+          <p>Checking</p>
+          <p>Checking</p>
+      </div>
+      <div>
+          <p>Checking</p>
+          <p>Checking</p>
+          <p>Checking</p>
+      </div>
+      <div>
+          <p>Checking</p>
+          <p>Checking</p>
+          <p>Checking</p>
+      </div>
+      <div>
+          <p>Checking</p>
+          <p>Checking</p>
+          <p>Checking</p>
+      </div>
+      <Footer />
+  </>
+);
+
+export default App;
