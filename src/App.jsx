735f5f38bef5a96585934531e7bc101d9b669bf5
Vlad Puschiulescu <vladpuschiulescu@gmail.com>
2022-05-25 13:54:07 +0300

    Delete test line

--- src/App.jsx
+++ src/App.jsx
@@ -31,7 +31,6 @@ const App = () => (
           } />
           <Route path="/login-form" element={
               <>
-                  <Header/>
                   <LoginForm/>
               </>
           } />
