5ec616e60087938fe454ee3a607715da52b8b40e
dragosvasile11 <vasile.dragos11@gmail.com>
2022-05-25 18:22:49 +0300

    CornerMap extends Component integrates the second map.

--- src/components/CornerMap.jsx
+++ src/components/CornerMap.jsx
@@ -6,12 +6,22 @@ const mapStyles = {
     height: '100%'
 };
 
-const CornerMap = () => {
-    return (
-        <>
-
-        </>
-    );
-};
+export class CornerMap extends Component {
+    render() {
+        return (
+            <Map
+                google={this.props.google}
+                zoom={14}
+                style={mapStyles}
+                initialCenter={
+                    {
+                        lat: -1.2884,
+                        lng: 36.8233
+                    }
+                }
+            />
+        );
+    }
+}
 
 export default CornerMap;
