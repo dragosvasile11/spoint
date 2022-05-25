7704ba1f320a7a3297bd4e6ee935dac612388315
dragosvasile11 <vasile.dragos11@gmail.com>
2022-05-25 13:55:36 +0300

    Delete unused code.

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,5 @@
 import React from "react";
 import { BrowserRouter as Router, Route, Routes} from "react-router-dom";
-import { Link } from "react-router-dom"
 
 import Header from "../src/components/Header";
 import Footer from "../src/components/Footer";
