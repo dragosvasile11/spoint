d65013aa30c40381b3a3486ced221a78b09db9de
dragosvasile11 <vasile.dragos11@gmail.com>
2022-05-26 10:39:36 +0300

    Add Modal component props and validation.

--- src/components/Modal.jsx
+++ src/components/Modal.jsx
@@ -2,7 +2,9 @@ import React from 'react';
 import CornerMap from "./CornerMap";
 
 
-const Modal = ({ children }) => {
+
+const Modal = ({ open, children , onClose}) => {
+    if (!open) return null
 
     return (
         <>
