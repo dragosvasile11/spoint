0f933258a9b0c10edc3dc2a1a847c443773ed422
dragosvasile11 <vasile.dragos11@gmail.com>
2022-05-26 18:21:34 +0300

    Create NavBar component.

---
+++ src/components/NavBar.jsx
@@ -0,0 +1,23 @@
+import React, { useState } from 'react';
+import {
+    MDBNavbar,
+    MDBContainer,
+    MDBIcon,
+    MDBNavbarNav,
+    MDBNavbarItem,
+    MDBNavbarLink,
+    MDBNavbarToggler,
+    MDBNavbarBrand,
+    MDBCollapse
+} from 'mdb-react-ui-kit';
+
+
+export default function NavBar() {
+    const [showNav, setShowNav] = useState(false);
+
+    return (
+        <>
+
+        </>
+    );
+}
