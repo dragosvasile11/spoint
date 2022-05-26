2cb6f7f7e37d7c035a014f587bc6d996e90d475d
dragosvasile11 <vasile.dragos11@gmail.com>
2022-05-26 10:38:35 +0300

    Add Modal component.

---
+++ src/components/Modal.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import CornerMap from "./CornerMap";
+
+
+const Modal = ({ children }) => {
+
+    return (
+        <>
+        </>
+
+    );
+};
+
+export default Modal;
