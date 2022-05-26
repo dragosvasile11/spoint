16c95b36daa2ae1f7c1575b9c13aa57be6cdc683
Vlad Puschiulescu <vladpuschiulescu@gmail.com>
2022-05-26 13:24:16 +0300

    Add links to sign in page

--- src/components/SignInForm.jsx
+++ src/components/SignInForm.jsx
@@ -17,8 +17,8 @@ function Copyright(props) {
     return (
         <Typography variant="body2" color="text.secondary" align="center" {...props}>
             {'Copyright © '}
-            <Link color="inherit" href="https://mui.com/">
-                Your Website
+            <Link color="inherit" to="/">
+                Spoint
             </Link>{' '}
             {new Date().getFullYear()}
             {'.'}
@@ -107,12 +107,12 @@ export default function SignInSide() {
                             </Button>
                             <Grid container>
                                 <Grid item xs>
-                                    <Link href="#" variant="body2">
+                                    <Link to="/" variant="body2">
                                         Forgot password?
                                     </Link>
                                 </Grid>
                                 <Grid item>
-                                    <Link href="#" variant="body2">
+                                    <Link to="/signUp-form" variant="body2">
                                         {"Don't have an account? Sign Up"}
                                     </Link>
                                 </Grid>
