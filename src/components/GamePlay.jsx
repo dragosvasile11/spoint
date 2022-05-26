ef62aa6a8ba1098b3bce5a358253db1e4491f9eb
dragosvasile11 <vasile.dragos11@gmail.com>
2022-05-26 15:25:30 +0300

    Add Card component.

--- src/components/GamePlay.jsx
+++ src/components/GamePlay.jsx
@@ -1,10 +1,11 @@
 import React from 'react';
+import Card from "./Card";
 
 const GamePlay = () => {
     return (
-        <div>
-            
-        </div>
+        <>
+            <Card />
+        </>
     );
 };
 
