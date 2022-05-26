f6f265ee1662c19f0ed9c3a8d8a60a5d037cb995
dragosvasile11 <vasile.dragos11@gmail.com>
2022-05-26 12:41:32 +0300

    Implement GoogleMap class, export GoogleApiWrapper.

---
+++ src/components/GoogleMap.jsx
@@ -0,0 +1,10 @@
+import React, {Component} from "react";
+import {GoogleApiWrapper, Map, Marker} from "google-maps-react";
+
+class GoogleMap extends Component {
+
+}
+
+export default GoogleApiWrapper({
+    apiKey: ''
+})(GoogleMap);
