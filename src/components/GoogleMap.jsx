09f1f93db06b919e77779335c99a8f7eb2eb8a0c
dragosvasile11 <vasile.dragos11@gmail.com>
2022-05-26 12:42:14 +0300

    Create constructor for GoogleMap class.

--- src/components/GoogleMap.jsx
+++ src/components/GoogleMap.jsx
@@ -2,7 +2,14 @@ import React, {Component} from "react";
 import {GoogleApiWrapper, Map, Marker} from "google-maps-react";
 
 class GoogleMap extends Component {
-
+    constructor(props) {
+        super(props);
+        this.state = {
+            markers: [
+            ]
+        };
+        this.onClick = this.onClick.bind(this);
+    }
 }
 
 export default GoogleApiWrapper({
