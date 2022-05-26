ddb7f0c6a288d46ae7bde18de29588b0ce029ade
Vlad Puschiulescu <vladpuschiulescu@gmail.com>
2022-05-27 00:49:42 +0300

    Import scroll up/down button

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,12 +1,12 @@
-import React from 'react';
 import Link from "@mui/material/Link";
+import ScrollUpDownButton from "./ScrollUpDownButton";
 
 
 const Footer = () => {
     return (
         <>
             <footer className="footer bg-dark text-center text-white fixed-bottom" style={{ backgroundColor: 'rgba(0, 0, 0, 0.1)', height: '13vh'}} >
-                <div className="container p-4 pb-0" >
+                <div id="footer-container" className="container p-4 pb-0">
                     <section className="mb-4">
                         <a className="btn btn-outline-light btn-floating m-1" href="#" role="button"
                         ><i className="fab fa-facebook-f"/></a>
@@ -20,6 +20,7 @@ const Footer = () => {
                         ><i className="fab fa-linkedin-in"/></a>
                         <a className="btn btn-outline-light btn-floating m-1" href="#" role="button"
                         ><i className="fab fa-github"/></a>
+                        <ScrollUpDownButton/>
                         <div className="text-center p-3">
                             © 2022 Copyright:
                             <Link className="text-warning" href="/about"> Spoint</Link>
