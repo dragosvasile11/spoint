0f7d355bde023d43c066cdb2c0a917fa45601483
dragosvasile11 <vasile.dragos11@gmail.com>
2022-05-26 10:39:53 +0300

    Add modal styles.

--- src/components/Modal.jsx
+++ src/components/Modal.jsx
@@ -1,6 +1,16 @@
 import React from 'react';
 import CornerMap from "./CornerMap";
 
+const MODAL_STYLES = {
+    position: 'fixed',
+    top: '50%',
+    left: '50%',
+    width: '900px',
+    height: '450px',
+    transform: 'translate(-50%, -50%)',
+    padding: '50px',
+    zIndex: 1000
+}
 
 
 const Modal = ({ open, children , onClose}) => {
