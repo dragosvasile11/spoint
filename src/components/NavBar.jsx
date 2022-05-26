0ee43d59d7d8729b500e80e4c65eca6e68bf6fc1
dragosvasile11 <vasile.dragos11@gmail.com>
2022-05-26 18:22:34 +0300

    Add navbar components.

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -17,7 +17,30 @@ export default function NavBar() {
 
     return (
         <>
-
+            <MDBNavbar expand='lg' dark bgColor='dark'>
+                <MDBContainer fluid>
+                    <MDBNavbarBrand href='#'>SPOINT</MDBNavbarBrand>
+                    <MDBNavbarToggler
+                        type='button'
+                        data-target='#navbarColor02'
+                        aria-controls='navbarColor02'
+                        aria-expanded='false'
+                        aria-label='Toggle navigation'
+                        onClick={() => setShowNav(!showNav)}
+                    >
+                        <MDBIcon icon='bars' fas />
+                    </MDBNavbarToggler>
+                    <MDBCollapse show={showNav} navbar id='navbarColor02'>
+                        <MDBNavbarNav className='me-auto mb-2 mb-lg-0'>
+                            <MDBNavbarItem className='active'>
+                                <MDBNavbarLink aria-current='page' href='#'>
+                                    <MDBIcon far icon="user" size="2x"/>
+                                </MDBNavbarLink>
+                            </MDBNavbarItem>
+                        </MDBNavbarNav>
+                    </MDBCollapse>
+                </MDBContainer>
+            </MDBNavbar>
         </>
     );
 }
