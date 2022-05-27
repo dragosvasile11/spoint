3afff478732f75c6996138f6584c73a000c393bd
Vlad Puschiulescu <vladpuschiulescu@gmail.com>
2022-05-27 03:38:26 +0300

    Implement sunglasses with hook

--- src/components/StreetViewMap.jsx
+++ src/components/StreetViewMap.jsx
@@ -2,6 +2,7 @@ import React, {useState}  from 'react';
 import ReactStreetview from "react-streetview";
 import {MDBBtn} from "mdb-react-ui-kit";
 import Modal from "./Modal";
+import SunglassesButton from "./SunglassesButton";
 
 
 const BUTTON_WRAPPER_STYLES = {
@@ -22,6 +23,11 @@ const OTHER_CONTENT_STYLES = {
 const StreetViewMap = () => {
     const googleMapsApiKey = '';
     const [isOpen, setIsOpen] = useState(false)
+    const [sunGlassesOn, setSunGlassesOn] = useState(false)
+
+    const sunglassesOnClick = () => {
+        setSunGlassesOn(!sunGlassesOn)
+    }
 
     const streetViewPanoramaOptions = {
         position: {lat: 44.439004, lng: 26.112520},
@@ -29,23 +35,27 @@ const StreetViewMap = () => {
         zoom: 1,
         zoomControl: true,
         addressControl: false,
-        fullscreenControl: false
+        fullscreenControl: false,
     };
     return (
         <>
             <div style={{
                 width: '100vw',
                 height: '100vh',
+                filter: 'invert(1)' + (sunGlassesOn ? "opacity(0.5)" : ""),
             }}>
                 <ReactStreetview
                     apiKey={googleMapsApiKey}
                     streetViewPanoramaOptions={streetViewPanoramaOptions}
                 />
             </div>
-            <div style={BUTTON_WRAPPER_STYLES}>
+            <div style={BUTTON_WRAPPER_STYLES} className="hover-button">
                 <MDBBtn rounded color='warning' onClick={() => setIsOpen(true)}>Give it a Guess!</MDBBtn>
+            </div>
+            <div>
                 <Modal open={isOpen} onClose={() => setIsOpen(false)}> modal </Modal>
             </div>
+            <SunglassesButton onClick={sunglassesOnClick}/>
         </>
 
     )
