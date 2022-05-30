34c71d94e2c0128af31162ddd5af1d20ca9fa25d
dragosvasile11 <vasile.dragos11@gmail.com>
2022-05-30 14:49:39 +0300

    Implement User class with fields.

---
+++ server/src/main/java/com/codecool/spoint/models/User.java
@@ -0,0 +1,10 @@
+package com.codecool.spoint.models;
+
+public class User {
+
+    private Long id;
+    private String firstName;
+    private String lastName;
+    private String email;
+    private String password;
+}
