b1887feeb8eff51cc0148a9a3be4c980f2081704
dragosvasile11 <vasile.dragos11@gmail.com>
2022-05-30 14:53:40 +0300

    Add constructor without id and NoArgsConstructor and AllArgsConstructor annotations.

--- server/src/main/java/com/codecool/spoint/models/User.java
+++ server/src/main/java/com/codecool/spoint/models/User.java
@@ -1,8 +1,13 @@
 package com.codecool.spoint.models;
 
+import lombok.AllArgsConstructor;
+import lombok.NoArgsConstructor;
+
 import javax.persistence.Entity;
 import javax.persistence.Id;
 
+@NoArgsConstructor
+@AllArgsConstructor
 @Entity(name = "User")
 public class User {
 
@@ -13,4 +18,11 @@ public class User {
     private String lastName;
     private String email;
     private String password;
+
+    public User(String firstName, String lastName, String email, String password) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.email = email;
+        this.password = password;
+    }
 }
