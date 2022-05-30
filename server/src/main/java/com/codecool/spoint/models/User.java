9beec0c6bb1ee7bb00167c13f2b89129daefcd8e
dragosvasile11 <vasile.dragos11@gmail.com>
2022-05-30 14:52:11 +0300

    Add @Entity to class and @Id attribute to id.

--- server/src/main/java/com/codecool/spoint/models/User.java
+++ server/src/main/java/com/codecool/spoint/models/User.java
@@ -1,8 +1,14 @@
 package com.codecool.spoint.models;
 
+import javax.persistence.Entity;
+import javax.persistence.Id;
+
+@Entity(name = "User")
 public class User {
 
+    @Id
     private Long id;
+
     private String firstName;
     private String lastName;
     private String email;
