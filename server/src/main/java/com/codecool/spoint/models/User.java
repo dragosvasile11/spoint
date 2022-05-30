e004992ef9fbcbc88a0a9a31ee8d56d106aef75a
dragosvasile11 <vasile.dragos11@gmail.com>
2022-05-30 14:57:32 +0300

    Add lombok annotations: Getter, Setter, Builder.

--- server/src/main/java/com/codecool/spoint/models/User.java
+++ server/src/main/java/com/codecool/spoint/models/User.java
@@ -1,11 +1,13 @@
 package com.codecool.spoint.models;
 
-import lombok.AllArgsConstructor;
-import lombok.NoArgsConstructor;
+import lombok.*;
 
 import javax.persistence.Entity;
 import javax.persistence.Id;
 
+@Getter
+@Setter
+@Builder
 @NoArgsConstructor
 @AllArgsConstructor
 @Entity(name = "User")
