ce5da0dbf3f3669f9ebbe69b264a8137fdb77edc
dragosvasile11 <vasile.dragos11@gmail.com>
2022-05-30 15:47:46 +0300

    Implement PlayerService as @Service, create field, constructor and methods.

--- server/src/main/java/com/codecool/spoint/services/PlayerService.java
+++ server/src/main/java/com/codecool/spoint/services/PlayerService.java
@@ -1,4 +1,28 @@
 package com.codecool.spoint.services;
 
+import com.codecool.spoint.models.Player;
+import com.codecool.spoint.repositories.PlayerRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
 public class PlayerService {
+
+    private final PlayerRepository playerRepository;
+
+    @Autowired
+    public PlayerService(PlayerRepository playerRepository) {
+        this.playerRepository = playerRepository;
+    }
+
+    public List<Player> getAllPlayers() {
+        return playerRepository.findAll();
+    }
+
+    public Optional<Player> getPlayerById(Long id) {
+        return playerRepository.findById(id);
+    }
 }
