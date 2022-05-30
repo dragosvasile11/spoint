ac5b4e8b29ef97ed713c548968d6c93ca03b01e6
Vlad Puschiulescu <vladpuschiulescu@gmail.com>
2022-05-31 00:51:32 +0300

    Create league model

---
+++ server/src/main/java/com/codecool/spoint/models/League.java
@@ -0,0 +1,49 @@
+package com.codecool.spoint.models;
+
+import lombok.*;
+
+import javax.persistence.*;
+import java.util.ArrayList;
+import java.util.List;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity(name = "league")
+public class League {
+
+    @Id
+    @SequenceGenerator(
+            name = "league_sequence",
+            sequenceName = "league_sequence",
+            allocationSize = 1
+    )
+
+    @GeneratedValue(
+            strategy = GenerationType.SEQUENCE,
+            generator = "league_sequence"
+    )
+
+    @Column(name = "id", updatable = false)
+    private long id;
+
+    @Column(name = "name", nullable = false, columnDefinition = "TEXT")
+    private String name;
+
+    @Column(name = "members", nullable = true, columnDefinition = "TEXT")
+    private List<Player> members;
+
+    @Column(name = "league_creator", nullable = false, columnDefinition = "TEXT")
+    private Player creator;
+
+    @Column(name = "league_score", nullable = true, columnDefinition = "INTEGER")
+    private int league_score;
+
+    public League(String name, Player creator) {
+        this.name = name;
+        this.members = new ArrayList<>();
+        this.creator = creator;
+        this.league_score = 0;
+    }
+}
