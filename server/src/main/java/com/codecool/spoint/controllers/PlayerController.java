8cce12daa171a24182dcd3c537cdf97b72fbbaf1
dragosvasile11 <vasile.dragos11@gmail.com>
2022-05-30 15:45:47 +0300

    Add @RestController, @RequestMapping, field, constructor.

--- server/src/main/java/com/codecool/spoint/controllers/PlayerController.java
+++ server/src/main/java/com/codecool/spoint/controllers/PlayerController.java
@@ -1,4 +1,26 @@
 package com.codecool.spoint.controllers;
 
+import com.codecool.spoint.models.Player;
+import com.codecool.spoint.services.PlayerService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+import java.util.Optional;
+
+@RestController
+@RequestMapping(path = "/players")
 public class PlayerController {
+
+    private final PlayerService playerService;
+
+    @Autowired
+    public PlayerController(PlayerService playerService) {
+        this.playerService = playerService;
+    }
+
+
 }
