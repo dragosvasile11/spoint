67b26f27b08cce900034eed3c339791fb317d5cc
Vlad Puschiulescu <vladpuschiulescu@gmail.com>
2022-06-03 02:56:50 +0300

    Wrap everything in theme context, theme provide and global styles to change theme mode

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -22,53 +22,60 @@ const App = () => {
         client.setCookie("theme", theme)
     }, [theme, setTheme])
 
-const App = () => (
+    return (
+
   <Router>
-      <Routes>
-          <Route path="/" element={
-              <>
-                  <Header/>
-                  <p>Checking</p>
-                  <p>Checking</p>
-                  <p>Checking</p>
-                  <Card/>
-                  <div>
-                    <p>Checking</p>
-                      <p>Checking</p>
-                      <p>Checking</p>
-                      <p>Checking</p>
-                      <p>Checking</p>
-                      <p>Checking</p>
-                      <p>Checking</p>
-                  </div>
-                  <Footer />
-                  <a id="bottom">SOME TEXT HERE</a>
-              </>
-          } />
-          <Route path="/signUp-form" element={
-              <>
-                  <SignUpForm/>
-              </>
-          } />
-          <Route path="/signIn-form" element={
-              <>
-                  <SignInForm/>
-              </>
-          } />
-          <Route path="/about" element={
-              <>
-                  <h1>ABOUT PAGE</h1>
-              </>
-          } />
-          <Route path="/guess" element = {
-                  <StreetViewMap />
-          } />
-          <Route path="/gameplay" element = {
-              <>
-              <GamePlay />
-              </>
-          } />
-      </Routes>
+      <ThemeContext.Provider value={ { theme, setTheme } }>
+          <ThemeProvider theme = {theme === "light" ? lightTheme : darkTheme}>
+              <GlobalStyles/>
+              <Routes>
+                  <Route path="/" element={
+                      <>
+                          <Header/>
+                          <p>Checking</p>
+                          <p>Checking</p>
+                          <Link to={"/gameplay"}><Button type={"button"}>USERPAGE</Button></Link>
+                          <p>Checking</p>
+                          <Card/>
+                          <div>
+                            <p>Checking</p>
+                              <p>Checking</p>
+                              <p>Checking</p>
+                              <p>Checking</p>
+                              <p>Checking</p>
+                              <p>Checking</p>
+                              <p>Checking</p>
+                          </div>
+                          <Footer />
+                          <a id="bottom">SOME TEXT HERE</a>
+                      </>
+                  } />
+                  <Route path="/signUp-form" element={
+                      <>
+                          <SignUpForm/>
+                      </>
+                  } />
+                  <Route path="/signIn-form" element={
+                      <>
+                          <SignInForm/>
+                      </>
+                  } />
+                  <Route path="/about" element={
+                      <>
+                          <h1>ABOUT PAGE</h1>
+                      </>
+                  } />
+                  <Route path="/guess" element = {
+                          <StreetViewMap />
+                  } />
+                  <Route path="/gameplay" element = {
+                      <>
+                      <GamePlay />
+                      </>
+                  } />
+              </Routes>
+          </ThemeProvider>
+      </ThemeContext.Provider>
   </Router>
 );
 
