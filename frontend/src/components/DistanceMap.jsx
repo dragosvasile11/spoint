d35777abbd92a76aa7291dc880aede46e29a8af4
dragosvasile11 <vasile.dragos11@gmail.com>
2022-06-03 08:02:24 +0300

    Implement hook with useState([]).

--- frontend/src/components/DistanceMap.jsx
+++ frontend/src/components/DistanceMap.jsx
@@ -5,6 +5,7 @@ import { useLocation } from 'react-router-dom'
 const DistanceMap = () => {
     const location = useLocation()
     const { from } = location.state
+    const [clicks, setClicks] = React.useState([]);
     return (
         <>
         </>
