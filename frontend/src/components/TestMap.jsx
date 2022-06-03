635760adee7d648528db746fa6ea2fc81619ed30
dragosvasile11 <vasile.dragos11@gmail.com>
2022-06-03 08:20:28 +0300

    Define const from props.

--- frontend/src/components/TestMap.jsx
+++ frontend/src/components/TestMap.jsx
@@ -6,6 +6,9 @@ import {Link} from "react-router-dom";
 const GuessMap = ({coords}) => {
     const [clicks, setClicks] = React.useState([]);
     const [isClicked, setIsClicked] = useState(false)
+    const streetViewLat = coords.lat;
+    const streetViewLng = coords.lng;
+
     return (
         <>
         </>
