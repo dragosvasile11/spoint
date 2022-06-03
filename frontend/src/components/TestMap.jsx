8cd89c58782336d00c3c86c43f96922421b6392a
dragosvasile11 <vasile.dragos11@gmail.com>
2022-06-03 08:24:28 +0300

    Add containerStyle css.

--- frontend/src/components/TestMap.jsx
+++ frontend/src/components/TestMap.jsx
@@ -3,6 +3,11 @@ import { GoogleMap, LoadScript, Marker } from '@react-google-maps/api';
 import {MDBBtn} from "mdb-react-ui-kit";
 import {Link} from "react-router-dom";
 
+const containerStyle = {
+    width: '500px',
+    height: '400px',
+};
+
 const GuessMap = ({coords}) => {
     const [clicks, setClicks] = React.useState([]);
     const [isClicked, setIsClicked] = useState(false)
