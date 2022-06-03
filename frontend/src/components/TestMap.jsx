9587014eed376b6f31ab92de6f67eaa0f19db3d2
dragosvasile11 <vasile.dragos11@gmail.com>
2022-06-03 08:18:52 +0300

    Imported useState hook, button component, Link from router-dom.

--- frontend/src/components/TestMap.jsx
+++ frontend/src/components/TestMap.jsx
@@ -1,4 +1,6 @@
-import React from 'react'
+import React, {useState} from 'react'
+import {MDBBtn} from "mdb-react-ui-kit";
+import {Link} from "react-router-dom";
 
 const GuessMap = ({coords}) => {
     return (
