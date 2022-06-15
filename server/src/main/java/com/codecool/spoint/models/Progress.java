1898dbda18c0c8c4b656b8c9f82ec465db5bdb55
dragosvasile11 <vasile.dragos11@gmail.com>
2022-06-15 11:07:44 +0300

    Implement @SequenceGenerator, OneToOne relationship, constructor.

--- server/src/main/java/com/codecool/spoint/models/Progress.java
+++ server/src/main/java/com/codecool/spoint/models/Progress.java
@@ -14,4 +14,35 @@ import javax.persistence.*;
 @Table(name = "progress")
 public class Progress {
 
+    @Id
+    @SequenceGenerator(
+            name = "progress_sequence",
+            sequenceName = "progress_sequence",
+            allocationSize = 1
+    )
+    @GeneratedValue(
+            strategy = GenerationType.SEQUENCE,
+            generator = "progress_sequence"
+    )
+    @Column(name = "id", updatable = false)
+    private Long id;
+
+    @Column(name = "famous_place_stage", nullable = false)
+    private int famousPlaceStage;
+
+    @Column(name = "romania_stage", nullable = false)
+    private int romaniaStage;
+
+    @Column(name = "world_stage", nullable = false)
+    private int worldStage;
+
+    @OneToOne(cascade = CascadeType.ALL, mappedBy = "progress")
+    @JsonIgnore
+    private Player player;
+
+    public Progress(int famousPlaceStage, int romaniaStage, int worldStage) {
+        this.famousPlaceStage = famousPlaceStage;
+        this.romaniaStage = romaniaStage;
+        this.worldStage = worldStage;
+    }
 }
