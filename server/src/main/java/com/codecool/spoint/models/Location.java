3990bce0a54ae012475f2f42afc0e772c781ce1f
dragosvasile11 <vasile.dragos11@gmail.com>
2022-06-15 11:05:03 +0300

    Create Location Entity.

---
+++ server/src/main/java/com/codecool/spoint/models/Location.java
@@ -0,0 +1,16 @@
+package com.codecool.spoint.models;
+
+import lombok.*;
+
+import javax.persistence.*;
+
+@Getter
+@Setter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity(name = "Location")
+@Table(name = "locations")
+public class Location {
+
+}
