21a2c7102569321b57da4bb85e2a38dda614b733
dragosvasile11 <vasile.dragos11@gmail.com>
2022-06-15 11:03:33 +0300

    Add @Bean CommandLineRunner with Location entities.

--- server/src/main/java/com/codecool/spoint/config/InitialConfig.java
+++ server/src/main/java/com/codecool/spoint/config/InitialConfig.java
@@ -1,9 +1,13 @@
 package com.codecool.spoint.config;
 
 import com.codecool.spoint.models.League;
+import com.codecool.spoint.models.Location;
 import com.codecool.spoint.models.Player;
+import com.codecool.spoint.models.Progress;
 import com.codecool.spoint.repositories.LeagueRepository;
+import com.codecool.spoint.repositories.LocationRepository;
 import com.codecool.spoint.repositories.PlayerRepository;
+import com.codecool.spoint.repositories.ProgressRepository;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -40,4 +44,15 @@ public class InitialConfig {
         };
     }
 
+    @Bean
+    CommandLineRunner commandLineRunnerLocation (LocationRepository locationRepository) {
+        return args -> {
+            Location location1 = new Location(41.88681699884825, 12.489867356141033);
+            Location location2 = new Location(51.500232577452806, -0.1280190147592774);
+            Location location3 = new Location(41.401493377336934, 2.172874982126398);
+            Location location4 = new Location(40.70560573166003, -74.00791460008375);
+            Location location5 = new Location(44.39029978067226, 26.07444242736911);
+            locationRepository.saveAll(List.of(location1, location2, location3, location4, location5));
+        };
+    }
 }
