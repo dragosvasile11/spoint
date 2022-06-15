1ecf162e62067d6fca9eb56dd01a0f5b3de8500b
dragosvasile11 <vasile.dragos11@gmail.com>
2022-06-15 11:04:35 +0300

    Add OneToOne relationship.

--- server/src/main/java/com/codecool/spoint/models/Player.java
+++ server/src/main/java/com/codecool/spoint/models/Player.java
@@ -51,6 +51,10 @@ public class Player {
     @JsonIgnore
     private League createdBy;
 
+    @OneToOne
+    @JoinColumn(name = "progress_id")
+    private Progress progress;
+
     public Player(String firstName, String lastName, String email, String password) {
         this.firstName = firstName;
         this.lastName = lastName;
