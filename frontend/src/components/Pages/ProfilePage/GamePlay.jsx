e7c3211c73e28526d3b90e20f83845f5d948c343
Vlad Puschiulescu <vladpuschiulescu@gmail.com>
2022-06-17 00:24:06 +0300

    Implement title and background for gameplay page

--- frontend/src/components/Pages/ProfilePage/GamePlay.jsx
+++ frontend/src/components/Pages/ProfilePage/GamePlay.jsx
@@ -2,17 +2,26 @@ import React from 'react';
 import Card from "../../Assets/Card";
 import NavBar from "./NavBar";
 
-const componentsStyle = {
-    backgroundColor: '#383333'
-}
-
 const GamePlay = ({progress}) => {
+
     return (
         <div style={componentsStyle}>
             <NavBar />
+            <h2 style={textAlignment}><b><i>Start your <p style={{ color: "#FFA900", display: "inline"}}>SPoint</p> journey here !</i></b></h2>
             <Card progress={progress}/>
         </div>
     );
 };
 
+const componentsStyle = {
+    backgroundImage: "url(/b.png)",
+    height: "100vh",
+    backgroundRepeat: "no-repeat",
+    backgroundSize: "100% 100%",
+}
+
+const textAlignment = {
+    textAlign: "center",
+    padding: "0.5%"
+}
 export default GamePlay;
