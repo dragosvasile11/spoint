d4b51fb9f2623ac4ee1e1a00253299df8f7d6666
Vlad Puschiulescu <vladpuschiulescu@gmail.com>
2022-06-16 17:04:27 +0300

    Change footer position

--- frontend/src/components/Pages/LandingPage/Footer.jsx
+++ frontend/src/components/Pages/LandingPage/Footer.jsx
@@ -8,12 +8,15 @@ import ScrollUpDownButton from "../../Buttons/ScrollUpDownButton";
 import Link from "@mui/material/Link";
 
 const footerStyles = {
-    backgroundColor: 'rgba(0, 0, 0, 0.1)', height: '13vh'
+    backgroundColor: 'rgba(0, 0, 0, 0.1)',
+    height: '15vh',
+    display: "inline-block",
+    width: "100%"
 }
 
 export default function App() {
     return (
-        <MDBFooter className='bg-dark text-center text-white fixed-bottom' style={footerStyles}>
+        <MDBFooter className='bg-dark text-center text-white' style={footerStyles}>
             <MDBContainer id="footer-container" className='p-4 pb-0'>
                 <section className='mb-4'>
                     <a className='btn btn-outline-light btn-floating m-1' href='src/components/Pages/LandingPage/Footer#' role='button'>
@@ -38,8 +41,7 @@ export default function App() {
                     <a className='btn btn-outline-light btn-floating m-1' href='https://github.com/CodecoolGlobal/el-proyecte-grande-sprint-1-java-dragosvasile11' role='button'>
                         <MDBIcon fab icon='github' />
                     </a>
-                    <ScrollUpDownButton/>
-                    <div className='text-center p-3' style={{ backgroundColor: 'rgba(0, 0, 0, 0.2)' }}>
+                    <div className='text-center p-3'>
                         © {new Date().getFullYear()} Copyright:
                         <Link className="text-warning" href="/src/components/Pages/About">
                             Spoint
