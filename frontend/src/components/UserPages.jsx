07a6991f5ea52b9105e859469513b80758a53bc0
Vlad Puschiulescu <vladpuschiulescu@gmail.com>
2022-06-17 06:36:46 +0300

    Refactor methods to protect routes

--- frontend/src/components/UserPages.jsx
+++ frontend/src/components/UserPages.jsx
@@ -1,10 +1,10 @@
 import React from 'react';
-import {getCookie} from "./Contexts/Cookies";
+import {getCookieObject} from "./Contexts/Cookies";
 import {Outlet, Navigate} from "react-router";
 
 const UserPages = () => {
 
-    return getCookie("loginToken") ? <Outlet/> : <Navigate to={"/"} />
+    return getCookieObject("loginToken") ? <Outlet/> : <Navigate to={"/"} />
 }
 
 export default UserPages;
--- frontend/src/components/VisitorPages.jsx
+++ frontend/src/components/VisitorPages.jsx
@@ -1,10 +1,10 @@
 import React from 'react';
-import {getCookie} from "./Contexts/Cookies";
+import {getCookieObject} from "./Contexts/Cookies";
 import {Outlet, Navigate} from "react-router";
 
 const VisitorPages = () => {
 
-    return getCookie("loginToken") ? <Navigate to={"../gameplay/"}/> : <Outlet/>
+    return getCookieObject("loginToken") ? <Navigate to={"../gameplay/"}/> : <Outlet/>
 }
 
 export default VisitorPages;
