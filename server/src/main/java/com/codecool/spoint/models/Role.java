8688d4874180d0b6814148d3ec49c87920c863cf
dragosvasile11 <vasile.dragos11@gmail.com>
2022-06-22 18:28:29 +0300

    Create Role Entity.

---
+++ server/src/main/java/com/codecool/spoint/models/Role.java
@@ -0,0 +1,16 @@
+package com.codecool.spoint.models;
+
+import lombok.*;
+
+import javax.persistence.*;
+
+@Getter
+@Setter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity(name = "Role")
+@Table(name = "roles")
+public class Role {
+
+}
