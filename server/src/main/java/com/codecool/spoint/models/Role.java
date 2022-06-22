e77dbb5a060c492fbad63cb1311c5911a2ac7fc0
dragosvasile11 <vasile.dragos11@gmail.com>
2022-06-22 18:29:27 +0300

    Add Role fields.

--- server/src/main/java/com/codecool/spoint/models/Role.java
+++ server/src/main/java/com/codecool/spoint/models/Role.java
@@ -12,5 +12,16 @@ import javax.persistence.*;
 @Entity(name = "Role")
 @Table(name = "roles")
 public class Role {
-
+    @Id
+    @SequenceGenerator(
+            name = "role_sequence",
+            sequenceName = "role_sequence",
+            allocationSize = 1
+    )
+    @GeneratedValue(
+            strategy = GenerationType.SEQUENCE,
+            generator = "role_sequence"
+    )
+    private Long id;
+    private String name;
 }
