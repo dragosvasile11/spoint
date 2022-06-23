568ed3695e69229fd5c412922a307026ec262762
Vlad Puschiulescu <vladpuschiulescu@gmail.com>
2022-06-24 02:22:27 +0300

    Comment drawer component for further testing

--- frontend/src/components/Pages/ProfilePage/GamePlay.jsx
+++ frontend/src/components/Pages/ProfilePage/GamePlay.jsx
@@ -1,13 +1,29 @@
-import React from 'react';
+import React, {useState} from 'react';
 import Card from "../../Assets/Card";
 import NavBar from "./NavBar";
 import Switch from "../../Buttons/SwitchTheme/Switch";
+import LeagueDrawer from "../../Assets/LeagueDrawer";
 
 const GamePlay = ({progress}) => {
+
+    const[drawerOpen, setDrawerOpen] = useState(false)
+
+    // const toggleDrawer = () => {
+    //     setDrawerOpen(!drawerOpen)
+    // }
+
+    // const toggleDrawer = (anchor, open) => (event) => {
+    //     if (event.type === 'keydown' && (event.key === 'Tab' || event.key === 'Shift')) {
+    //         return;
+    //     }
+
+        // setDrawerOpen({ ...drawerOpen, [anchor]: open });
+    // };
 
     return (
         <div style={componentsStyle}>
             <NavBar />
+            {/*<LeagueDrawer isOpen={drawerOpen} toggleDrawer={toggleDrawer}/>*/}
             <h2 style={textAlignment}><b><i>Start your <p style={{ color: "#FFA900", display: "inline"}}>SPoint</p> journey here !</i></b></h2>
             <Card progress={progress}/>
             <Switch/>
