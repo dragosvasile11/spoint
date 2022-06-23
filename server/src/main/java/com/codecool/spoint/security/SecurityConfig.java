7defc3682294cf527b62e39d6bcf62a773f48275
dragosvasile11 <vasile.dragos11@gmail.com>
2022-06-23 08:36:55 +0300

    Add configure http method.

--- server/src/main/java/com/codecool/spoint/security/SecurityConfig.java
+++ server/src/main/java/com/codecool/spoint/security/SecurityConfig.java
@@ -3,6 +3,7 @@ package com.codecool.spoint.security;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.core.userdetails.UserDetailsService;
@@ -19,5 +20,11 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(AuthenticationManagerBuilder auth) throws Exception {
         auth.userDetailsService(userDetailsService).passwordEncoder(bCryptPasswordEncoder);
+    }
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http
+                .csrf().disable();
     }
 }
