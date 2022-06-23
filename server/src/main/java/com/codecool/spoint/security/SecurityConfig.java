d29961ee5ee905e3709800ab722ba621bf7646b6
dragosvasile11 <vasile.dragos11@gmail.com>
2022-06-23 08:34:05 +0300

    Create SecurityConfig with EnableWebSecurity, UserDetailsService, encoder.

---
+++ server/src/main/java/com/codecool/spoint/security/SecurityConfig.java
@@ -0,0 +1,17 @@
+package com.codecool.spoint.security;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+
+@Configuration
+@EnableWebSecurity
+@RequiredArgsConstructor
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+    private final UserDetailsService userDetailsService;
+    private final BCryptPasswordEncoder bCryptPasswordEncoder;
+}
