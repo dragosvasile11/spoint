616508ccf5165dade4a0a81b5ce6773eb0b786d1
Vlad Puschiulescu <vladpuschiulescu@gmail.com>
2022-06-24 02:20:15 +0300

    Add copyright component - bottom pages text

---
+++ frontend/src/components/Assets/SpointCopyright.jsx
@@ -0,0 +1,25 @@
+import React from 'react';
+import Typography from "@mui/material/Typography";
+import Link from "@mui/material/Link";
+
+function SpointCopyright(props, {color}) {
+
+    return (
+        <>
+
+            <Typography variant="body2" color="text.secondary" align="center" fontSize={"1vw"} {...props}>
+                {'Copyright © '}
+                <Link color={ color ? color : "inherit"}
+                      href="/about"
+                      className="text-warning"
+                >
+                    Spoint
+                </Link>{' '}
+                {new Date().getFullYear()}
+                {'.'}
+            </Typography>
+        </>
+    );
+}
+
+export default SpointCopyright;
