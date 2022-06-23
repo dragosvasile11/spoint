4d0a9f40cc5e341003ae9d34d712c8cc0b7b7b57
dragosvasile11 <vasile.dragos11@gmail.com>
2022-06-23 08:35:38 +0300

    Implement configure method for Auth Manager Builder.

--- server/src/main/java/com/codecool/spoint/security/SecurityConfig.java
+++ server/src/main/java/com/codecool/spoint/security/SecurityConfig.java
@@ -2,6 +2,7 @@ package com.codecool.spoint.security;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.core.userdetails.UserDetailsService;
@@ -14,4 +15,9 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
     private final UserDetailsService userDetailsService;
     private final BCryptPasswordEncoder bCryptPasswordEncoder;
+
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth.userDetailsService(userDetailsService).passwordEncoder(bCryptPasswordEncoder);
+    }
 }
