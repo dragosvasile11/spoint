e370cfd42953aa9e0e381b74220ff547091d7ee7
dragosvasile11 <vasile.dragos11@gmail.com>
2022-06-23 10:22:07 +0300

    Implement UserDetailsService and Override method loadUserByUsername.

--- server/src/main/java/com/codecool/spoint/services/PlayerService.java
+++ server/src/main/java/com/codecool/spoint/services/PlayerService.java
@@ -6,20 +6,23 @@ import com.codecool.spoint.repositories.PlayerRepository;
 import com.codecool.spoint.repositories.RoleRepository;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 import org.springframework.util.ReflectionUtils;
 
 import javax.transaction.Transactional;
 import java.lang.reflect.Field;
-import java.util.List;
-import java.util.Map;
-import java.util.Optional;
+import java.util.*;
 
 @Service
 @Transactional
 @Slf4j
-public class PlayerService {
+public class PlayerService implements UserDetailsService {
 
     private final PlayerRepository playerRepository;
     private final RoleRepository roleRepository;
@@ -30,6 +33,23 @@ public class PlayerService {
         this.playerRepository = playerRepository;
         this.roleRepository = roleRepository;
         this.passwordEncoder = passwordEncoder;
+    }
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        Player player = playerRepository.findByUsername(username);
+        if(player == null) {
+            log.error("User not found in the database");
+            throw new UsernameNotFoundException("User not found in the database");
+        }
+        else {
+            log.info("User found in the database: {}", username);
+        }
+        Collection<SimpleGrantedAuthority> authorities = new ArrayList<>();
+        player.getRoles().forEach(role -> {
+            authorities.add(new SimpleGrantedAuthority(role.getName()));
+        });
+        return new User(player.getEmail(), player.getPassword(), authorities);
     }
 
     public List<Player> getAllPlayers() {
