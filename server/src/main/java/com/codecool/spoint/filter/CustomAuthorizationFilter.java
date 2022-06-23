adfbab86df91b6d339300ff52f2b44fe44b00744
dragosvasile11 <vasile.dragos11@gmail.com>
2022-06-23 08:26:52 +0300

    Add SimpleGrantedAuthority for each Role of the User.

--- server/src/main/java/com/codecool/spoint/filter/CustomAuthorizationFilter.java
+++ server/src/main/java/com/codecool/spoint/filter/CustomAuthorizationFilter.java
@@ -5,6 +5,7 @@ import com.auth0.jwt.JWTVerifier;
 import com.auth0.jwt.algorithms.Algorithm;
 import com.auth0.jwt.interfaces.DecodedJWT;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.web.filter.OncePerRequestFilter;
 
 import javax.servlet.FilterChain;
@@ -12,7 +13,10 @@ import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Collection;
 
+import static java.util.Arrays.stream;
 import static org.springframework.http.HttpHeaders.AUTHORIZATION;
 
 @Slf4j
@@ -36,6 +40,11 @@ public class CustomAuthorizationFilter extends OncePerRequestFilter {
 
                     String username = decodedJWT.getSubject();
                     String[] roles = decodedJWT.getClaim("roles").asArray(String.class);
+
+                    Collection<SimpleGrantedAuthority> authorities = new ArrayList<>();
+                    stream(roles).forEach(role -> {
+                        authorities.add(new SimpleGrantedAuthority(role));
+                    });
                 } catch (Exception exception){
 
                 }
