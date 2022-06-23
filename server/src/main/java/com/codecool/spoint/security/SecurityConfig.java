075ce5a28a564a9fee1a1f6da83101cf0ac95e5e
dragosvasile11 <vasile.dragos11@gmail.com>
2022-06-23 08:42:18 +0300

    Insert authenticationManagerBean method.

--- server/src/main/java/com/codecool/spoint/security/SecurityConfig.java
+++ server/src/main/java/com/codecool/spoint/security/SecurityConfig.java
@@ -3,7 +3,9 @@ package com.codecool.spoint.security;
 import com.codecool.spoint.filter.CustomAuthenticationFilter;
 import com.codecool.spoint.filter.CustomAuthorizationFilter;
 import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
@@ -45,5 +47,11 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
                 .and()
                 .addFilter(new CustomAuthenticationFilter(authenticationManagerBean()))
                 .addFilterBefore(new CustomAuthorizationFilter(), UsernamePasswordAuthenticationFilter.class);
+    }
+
+    @Bean
+    @Override
+    public AuthenticationManager authenticationManagerBean() throws Exception {
+        return super.authenticationManagerBean();
     }
 }
