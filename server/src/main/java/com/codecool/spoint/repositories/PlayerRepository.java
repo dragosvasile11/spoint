0a3cc7071cf411d823e4f12a59253c06219d2b25
dragosvasile11 <vasile.dragos11@gmail.com>
2022-07-09 08:40:57 +0300

    Add methods in PlayerRepository

--- server/src/main/java/com/codecool/spoint/repositories/PlayerRepository.java
+++ server/src/main/java/com/codecool/spoint/repositories/PlayerRepository.java
@@ -4,8 +4,12 @@ import com.codecool.spoint.models.Player;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
+import java.util.Optional;
 
 public interface PlayerRepository extends JpaRepository<Player, Long> {
     List<Player> findAllByOrderByIdAsc();
-    Player findByEmail(String email);
+    //    Player findByEmail(String email);
+    Optional<Player> findByUsername(String username);
+    List<Player> findAllBy();
+    boolean existsUserByUsername(String username);
 }
